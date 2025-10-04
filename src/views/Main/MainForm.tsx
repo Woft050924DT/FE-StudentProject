b8d70bdbd1bee015f25767245ddb290d702784e9
NguyenDuc9 <duc362005lx2005xl@gmail.com>
2025-10-04 22:25:50 +0700

    form student

--- src/views/Main/MainForm.tsx
+++ src/views/Main/MainForm.tsx
@@ -1,19 +1,15 @@
 // src/views/Main/MainForm.tsx
-import React from 'react';
+import React from "react";
 
 const MainForm: React.FC = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-white">
       <div className="max-w-2xl w-full p-6">
         <h1 className="text-2xl font-bold text-gray-900">Main Form</h1>
-        <p className="mt-2 text-gray-600">
-          Bạn đã đăng nhập thành công. Đây là màn hình chính.
-        </p>
+        <p className="mt-2 text-gray-600">hello wworld.</p>
       </div>
     </div>
   );
 };
 
 export default MainForm;
-
-
